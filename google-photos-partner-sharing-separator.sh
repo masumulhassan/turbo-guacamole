222c07e84ff2cb0c6bcab637ed3616f759312a08
Masumul Hassan <contact@masum.me>
2022-05-24 02:32:53 +0600

    Add jq for json parsing

--- google-photos-partner-sharing-separator.sh
+++ google-photos-partner-sharing-separator.sh
@@ -2,7 +2,6 @@
 
 #Usage bash google-photos-partner-sharing-separator.sh -i {Input Google Photos Folder Path} -o {Output path where partner shared file will be moved}
 
-
 #Reading input and output flags
 while getopts i:o: flag
 do
@@ -15,16 +14,16 @@ done
 find_partner_share_and_move() {
   echo "Original Metadata file: $1";
   if grep -q fromPartnerSharing "$1"; then
-      current_file=$(echo "$1" | sed 's/\.[^.]*$//');
+      current_file=$(jq '.title' "$1" | sed -r 's/^"|"$//g';);
       echo "Moving -> $current_file";
-      mv "$1" "$outputpath";
-      mv "$current_file" "$outputpath";
+	  DIR="$(dirname "${1}")";
+	  
+      mkdir -p "${outputpath}/${DIR}";
+	  mv "${1}" "${outputpath}/${DIR}/.";
+      mv "${DIR}/${current_file}" "${outputpath}/${DIR}/.";
   fi
 }
 
 find "$inputpath" -type f -name "*.json" -print0 | while read -d $'\0' file; do
     find_partner_share_and_move "$file"
 done
-
-
-
